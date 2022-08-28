7f2084ac493320eeb93ef06dcaba5eb34bf09d72
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-08-28 16:47:30 +0100

    Dummy Data

---
+++ src/data/Data.js
@@ -0,0 +1,56 @@
+const Data = [
+  {
+    id: 'm1',
+    title: 'Safari Wildlife Resort',
+    location: 'Kenya',
+    googleMapsURL:
+      'https://www.google.com/maps/search/kenya+wildlife+safari/@-2.0190568,34.9552358,7z',
+    startDate: '12 Jan 2021',
+    endDate: '21 Jan 2021',
+    description:
+      'The main attractions are frequently large animals from Sub-Saharan Africa which people can see in wildlife reserves such as giraffes, lions, white lions, rhinoceros, elephants, hippopotamus, zebras, ostriches, African buffalo, oryx, sometimes dromedary camels.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1520124442480-b5c60b0f80c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YWZyaWNhJTIwd2lsZGxpZmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 'm2',
+    title: 'Miami Beach',
+    location: 'USA',
+    googleMapsURL:
+      'https://www.google.com/maps/place/Miami+Beach,+FL,+USA/@25.8103793,-80.2101822,12z/data=!3m1!4b1!4m5!3m4!1s0x88d9a6172bfeddb9:0x37be1741259463eb!8m2!3d25.790654!4d-80.1300455',
+    startDate: '12 Feb 2021',
+    endDate: '19 Feb 2021',
+    description:
+      'Miami Beach is a south Florida island city, connected by bridges to mainland Miami. Wide beaches stretch from North Shore Open Space Park, past palm-lined Lummus Park to South Pointe Park.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1521245585918-35fd32bf376f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG1pYW1pJTIwYmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 'm3',
+    title: 'Al Seyahi Skysurf',
+    location: 'Dubai',
+    googleMapsURL:
+      'https://www.google.com/maps/place/Skydive+Dubai/@25.0909163,54.8741814,11z/data=!4m9!1m2!2m1!1sdubai+skydiving!3m5!1s0x3e5f1356e05554f1:0xb8c106f7a8a0554d!8m2!3d25.0909163!4d55.1378533!15sCg9kdWJhaSBza3lkaXZpbmdaESIPZHViYWkgc2t5ZGl2aW5nkgEQc2t5ZGl2aW5nX2NlbnRlcuABAA',
+    startDate: '12 May 2021',
+    endDate: '21 May 2021',
+    description:
+      'Parachuting, including also skydiving, is a method of transiting from a high point in the atmosphere to the surface of Earth with the aid of gravity, involving the control of speed during the descent using a parachute or parachutes.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1633694733756-d16604e6a1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNreWRpdmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 'm4',
+    title: 'Mount Bullar',
+    location: 'Australia',
+    googleMapsURL:
+      'https://www.google.com/maps/place/Mount+Buller+VIC+3723,+Australia/@-37.2676579,145.8925123,10z/data=!3m1!4b1!4m5!3m4!1s0x6b28824a10d37e07:0x40579a430a05470!8m2!3d-37.1465655!4d146.4506354',
+    startDate: '12 Dec 2021',
+    endDate: '18 Dec 2021',
+    description:
+      'Mount Buller is a resort village in eastern Victoria, Australia. It’s known for its downhill ski slopes and terrain parks. Bike trails and footpaths also crisscross the mountain',
+    imageUrl:
+      'https://images.unsplash.com/photo-1597251000118-e702d0828392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdW50YWluJTIwY2xpbWJpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  },
+];
+
+export default Data;
