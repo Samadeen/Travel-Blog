3d733948bcb1558642c10646141768da1e945fd3
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-08-28 16:48:39 +0100

    Linking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Data from './data/Data';
 
 function App() {
-  return <div className='App'></div>;
+  const content = Data.map((item) => {
+    return (
+      <Content
+        key={item.id}
+        id={item.id}
+        title={item.title}
+        location={item.location}
+        map={item.googleMapsURL}
+        startDate={item.startDate}
+        endDate={item.endDate}
+        description={item.description}
+        imageUrl={item.imageUrl}
+      />
+    );
+  });
+
+  console.log(content);
+
+  return (
+    <div className='App'>
+      <Header />
+      {content}
+    </div>
+  );
 }
 
 export default App;
